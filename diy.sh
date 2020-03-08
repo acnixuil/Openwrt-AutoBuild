abf354385452db940fdb43f9ba6fd5c2db756a65
e <acnixuil@gmail.com>
2020-03-09 02:05:26 +0800

    update

--- diy.sh
+++ diy.sh
@@ -8,5 +8,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/acnixuil/luci-theme-argon.git package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
+# git clone -b 18.06 https://github.com/acnixuil/luci-theme-argon.git package/lean/luci-theme-argon
