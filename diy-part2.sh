b7c0be7f65840611530260f39dd419bc7abcd73a
acnixuil <35956405+acnixuil@users.noreply.github.com>
2020-06-28 23:23:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.9/g" target/linux/x86/Makefile
+#sed -i "s/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.9/g" target/linux/x86/Makefile
