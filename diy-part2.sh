9f2935bf686f7b2eb288e6572b32dd54b19c02e7
acnixuil <acnixuil@gmail.com>
2020-07-19 15:14:14 +0800

    update action file

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,3 @@
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 #sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g" target/linux/x86/Makefile
-sed -i '$a src-git jerryk https://github.com/acnixuil/openwrt-package.git' feeds.conf.default
